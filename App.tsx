de91452c43bf86dfece1b92ef18b2132101fad30
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-07-22 05:58:06 -0300

    Lesson 3

--- App.tsx
+++ App.tsx
@@ -3,20 +3,20 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { THEME } from './src/styles/theme';
 
-import SignIn from './src/screens/SignIn';
+import { Routes } from './src/routes';
 import Loading from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
-  return (
+  return ( 
     <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle='light-content'
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Heading, Icon, VStack, useTheme } from "native-base";
+import { Platform } from "react-native";
+import { Heading, Icon, VStack, useTheme, KeyboardAvoidingView } from "native-base";
 import { Envelope, Key } from "phosphor-react-native";
 
 import Logo from "../assets/logo_primary.svg"
@@ -8,8 +9,8 @@ import Input from "../components/Input";
 import Button from "../components/Button";
 
 export default function SignIn() {
-  const [name, setname] = useState("")
-  const [password, setpassword] = useState("")
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
 
   const { colors } = useTheme()
 
@@ -18,28 +19,31 @@ export default function SignIn() {
   }
 
   return (
-    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-      <Logo />
-
-      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-        Acesse sua conta
-      </Heading>
-
-      <Input 
-        mb={4}
-        placeholder="E-mail"
-        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-        onChangeText={setname}
-      />
-      <Input
-        mb={8}
-        placeholder="Senha"
-        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
-        secureTextEntry
-        onChangeText={setpassword}
-      />
-
-      <Button title="Entrar" w="full" onPress={handleSignIn} />
-    </VStack>
+    <KeyboardAvoidingView
+      flex={1}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      bg="gray.600"
+    >
+      <VStack flex={1} justifyContent="center" alignItems="center" bg="gray.600" px={8} /* pt={24} */>
+        <Logo />
+        <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+          Acesse sua conta
+        </Heading>
+        <Input
+          mb={4}
+          placeholder="E-mail"
+          InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+          onChangeText={setName}
+        />
+        <Input
+          mb={8}
+          placeholder="Senha"
+          InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+          secureTextEntry
+          onChangeText={setPassword}
+        />
+        <Button title="Entrar" w="full" onPress={handleSignIn} />
+      </VStack>
+    </KeyboardAvoidingView>
   )
 }
